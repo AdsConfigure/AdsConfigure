614a01c1062e776702a8e756de115a848b495a71
AdsConfigure <84325204+AdsConfigure@users.noreply.github.com>
2021-07-11 23:15:14 +0800

    Update GetZhihuData.py

--- GetZhihuData.py
+++ GetZhihuData.py
@@ -104,7 +104,8 @@ if __name__=='__main__':
             time.sleep(1)
         except Exception as e:
             with open(readmePath, "w") as readme:
-                readme.write("Exception:"+str(e))
+                s="Exception:"+str(e)
+                readme.write(s)
             break
             pass
         
