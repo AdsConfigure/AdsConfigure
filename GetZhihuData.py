b155668ffdac22117a1b8896c3c4d3cd065a103d
AdsConfigure <84325204+AdsConfigure@users.noreply.github.com>
2021-07-11 21:54:19 +0800

    Update GetZhihuData.py

--- GetZhihuData.py
+++ GetZhihuData.py
@@ -7,18 +7,15 @@ handle = sys.argv[1]
 token = sys.argv[2]
 readmePath = sys.argv[3]
 
-# %%
-# https://github.com/egrcc/zhihu-python
-
 headers = {
-    'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36",
+    'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36 x-ab-param: li_edu_page=old;qap_question_author=0;qap_question_visitor= 0;tp_topic_style=0;pf_noti_entry_num=2;tp_dingyue_video=0;top_test_4_liguangyi=1;se_ffzx_jushen1=0;zr_slotpaidexp=1;tp_zrec=1;li_paid_answer_exp=0;tp_contents=1;li_sp_mqbk=0;li_vip_verti_search=0;li_panswer_topic=0;zr_expslotpaid=1;pf_adjust=1 x-ab-pb: CmJpAUMAVgzsCkcADwuEAk8BtQuQAmALtwB0ATQMnwLPC7QKBwzgC4gBUguJAuQKpgFrAUABWAHSATsC1wsbAIkM9At9AmoBmwuNAQoCTAu0AAELZwBtAtwLRQIqAlsCNww/ABIxAhUBAQABAAADAQACAAACCwABAAABAAAAAgEAAAAAAAAAAAECAAMAAAEAAAABAAABAA==",
     'Host': "www.zhihu.com",
     'Origin': "http://www.zhihu.com",
     'Pragma': "no-cache",
     'Referer': "http://www.zhihu.com/"
 }
 
-url = 'https://www.zhihu.com/people/guo-fei-16-12'
+url = 'https://www.zhihu.com/people/yy6969'
 
 r = requests.get(url, headers=headers, verify=False)
 soup = BeautifulSoup(r.content, "lxml")
